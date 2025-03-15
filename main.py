498a4d632900fe8e1a3c4e9ac88557f7807dbc49
Yihong Wang <bellowswang@gmail.com>
2025-03-15 15:39:49 +0100

    [TASK1-33] Minor changes

--- main.py
+++ main.py
@@ -18,31 +18,26 @@ if __name__ == '__main__':
         print(midi_file_name)
         file_path = os.path.join(folder_path, midi_file_name)
         try:
-            guitar_notes, key, ticks_per_beat = process_midi_file(file_path,
-                                                                  track_name='solo',
-                                                                  subdivision=sample_frequency_per_beat)
-            midi_data[midi_file_name] = {
-                'notes': guitar_notes,
-                'key': key,
-                'ticks_per_beat': ticks_per_beat
-            }
+            midi_data[midi_file_name] = process_midi_file(file_path,
+                                                          track_name='solo',
+                                                          subdivision=sample_frequency_per_beat)
         except:
             print('Cannot extract the tracks from the MIDI file.')
 
     midi_example = midi_data.get('AutumnLeaves.mid')
 
     generate_midi_from_tokens(tokens=midi_example['notes'],
-                              output_file_name='new_guitar_training.mid',
+                              output_file_name='training.mid',
                               ticks_per_beat=midi_example['ticks_per_beat'],
-                              subdivision=sample_frequency_per_beat)
+                              subdivision=midi_example['sample_steps_per_beat'])
 
     jazz_gpt = JazzGpt(trim_and_convert_nones(midi_example['notes'],
-                                              subdivision=sample_frequency_per_beat))
+                                              subdivision=midi_example['sample_steps_per_beat']))
     jazz_gpt.jazzgpt_training()
     generated_notes = jazz_gpt.generate_music_from_notes(initial_notes=[-1, -1, 57, 57, 62, 62],
                                                          next_n=2000)
 
     generate_midi_from_tokens(tokens=replace_neg1_with_none(generated_notes),
-                              output_file_name='new_guitar_generation_3.mid',
+                              output_file_name='generation.mid',
                               ticks_per_beat=midi_example['ticks_per_beat'],
-                              subdivision=sample_frequency_per_beat)
+                              subdivision=midi_example['sample_steps_per_beat'])
